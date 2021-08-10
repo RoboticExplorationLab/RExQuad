6625cb8d91ba63914890698f6d5d0a09335502a9
Alexander A Bouman <alex.bouman@gmail.com>
2021-08-10 16:51:56 -0400

    Merging

--- Arduino/MOTOR_nanopd/MOTOR_nanopd.ino
+++ Arduino/MOTOR_nanopd/MOTOR_nanopd.ino
@@ -17,6 +17,7 @@
 #define BACK_LEFT_PIN   (12)
 
 #define MAX_THROTLE     (1900)
+#define THROTlE_OFF     (1500)
 #define MIN_THROTLE     (1100)
 
 // Define the Motor ESC structs
@@ -26,33 +27,32 @@ bool light_on = true;
 // Build buffers and message types
 uint8_t motors_buffer[256];
 size_t motors_buffer_length = sizeof(motors_buffer);
-messaging_MOTORS MOTORS_input = {MIN_THROTLE, MIN_THROTLE, MIN_THROTLE, MIN_THROTLE, 0.0};
+messaging_MOTORS MOTORS_input = {THROTlE_OFF, THROTlE_OFF, THROTlE_OFF, THROTlE_OFF, 0.0};
 
 // Initialize packet serial ports
 PacketSerial jetsonPacketSerial;
+
+unsigned long last_time = millis();
 
 void onMotorsRecieved(const uint8_t *buffer, size_t size);
 void displayMessage(messaging_MOTORS &mes);
 void sendMessage(messaging_MOTORS &mes);
 void arm();
 void calibrate();
+void kill();
 
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
 
     // Setup Motor ESC
-    // pinMode(FRONT_LEFT_PIN, OUTPUT);
-    // pinMode(FRONT_RIGHT_PIN, OUTPUT);
-    // pinMode(BACK_RIGHT_PIN, OUTPUT);
-    // pinMode(BACK_LEFT_PIN, OUTPUT);
     front_left_esc.attach(FRONT_LEFT_PIN, MIN_THROTLE, MAX_THROTLE);
     front_right_esc.attach(FRONT_RIGHT_PIN, MIN_THROTLE, MAX_THROTLE);
     back_right_esc.attach(BACK_RIGHT_PIN, MIN_THROTLE, MAX_THROTLE);
     back_left_esc.attach(BACK_LEFT_PIN, MIN_THROTLE, MAX_THROTLE);
 
     // Setup PacketSerial to handle communicating from Serial
-    Serial.begin(115200);
+    Serial.begin(57600);
     while (!Serial) { delay(100); }
     jetsonPacketSerial.setStream(&Serial);
     jetsonPacketSerial.setPacketHandler(&onMotorsRecieved);
@@ -68,6 +68,18 @@ void loop() {
     if (DEBUG) {
         displayMessage(MOTORS_input);
     }
+
+    if ((last_time - millis()) > 1000) {
+        // kill();
+
+        if (light_on) {
+            digitalWrite(LED_PIN, HIGH);
+        }
+        else {
+            digitalWrite(LED_PIN, LOW);
+        }
+        light_on = !light_on;
+    }
 }
 
 /*
@@ -80,13 +92,15 @@ void onMotorsRecieved(const uint8_t *buffer, size_t size) {
         Serial.printf("\nDecoding failed: %s\n", PB_GET_ERROR(&stream));
     }
 
-    if (light_on) {
-        digitalWrite(LED_PIN, HIGH);
-    }
-    else {
-        digitalWrite(LED_PIN, LOW);
-    }
-    light_on = !light_on;
+    // if (light_on) {
+    //     digitalWrite(LED_PIN, HIGH);
+    // }
+    // else {
+    //     digitalWrite(LED_PIN, LOW);
+    // }
+    // light_on = !light_on;
+
+    last_time = millis();
 }
 
 void displayMessage(messaging_MOTORS &mes) {
@@ -101,38 +115,38 @@ void displayMessage(messaging_MOTORS &mes) {
 void sendMessage(messaging_MOTORS &mes) {
     /* Create a stream that will write to our buffer. */
 
-    if (MIN_THROTLE < mes.front_left && mes.front_left < MAX_THROTLE) {
+    if (THROTlE_OFF < mes.front_left && mes.front_left < MAX_THROTLE) {
         front_left_esc.writeMicroseconds((int) mes.front_left);
     }
-    if (MIN_THROTLE < mes.front_right && mes.front_right < MAX_THROTLE) {
+    if (THROTlE_OFF < mes.front_right && mes.front_right < MAX_THROTLE) {
         front_right_esc.writeMicroseconds((int) mes.front_left);
     }
-    if (MIN_THROTLE < mes.back_right && mes.back_right < MAX_THROTLE) {
+    if (THROTlE_OFF < mes.back_right && mes.back_right < MAX_THROTLE) {
         back_right_esc.writeMicroseconds((int) mes.back_right);
     }
-    if (MIN_THROTLE < mes.back_left && mes.back_left < MAX_THROTLE) {
+    if (THROTlE_OFF < mes.back_left && mes.back_left < MAX_THROTLE) {
         back_left_esc.writeMicroseconds((int) mes.back_left);
     }
 }
 
 void arm() {
-    front_left_esc.writeMicroseconds(1500);
-    front_right_esc.writeMicroseconds(1500);
-    back_right_esc.writeMicroseconds(1500);
-    back_left_esc.writeMicroseconds(1500);
+    kill();
     delay(2000);
+
     front_left_esc.writeMicroseconds(MAX_THROTLE);
     front_right_esc.writeMicroseconds(MAX_THROTLE);
     back_right_esc.writeMicroseconds(MAX_THROTLE);
     back_left_esc.writeMicroseconds(MAX_THROTLE);
-    delay(5000);
-    front_left_esc.writeMicroseconds(1500);
-    front_right_esc.writeMicroseconds(1500);
-    back_right_esc.writeMicroseconds(1500);
-    back_left_esc.writeMicroseconds(1500);
+    delay(7000);
+
+    kill();
     delay(2000);
 }
 
-void calibrate() {
+void kill() {
+    front_left_esc.writeMicroseconds(THROTlE_OFF);
+    front_right_esc.writeMicroseconds(THROTlE_OFF);
+    back_right_esc.writeMicroseconds(THROTlE_OFF);
+    back_left_esc.writeMicroseconds(THROTlE_OFF);
 }
 
