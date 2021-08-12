2f5763b570b257addd360b86c2ff4c8c1cc1c33a
Alexander A Bouman <alex.bouman@gmail.com>
2021-08-12 09:41:12 -0400

    Working Motors

--- Arduino/MOTOR_nanopd/MOTOR_nanopd.ino
+++ Arduino/MOTOR_nanopd/MOTOR_nanopd.ino
@@ -16,9 +16,8 @@
 #define BACK_RIGHT_PIN  (11)
 #define BACK_LEFT_PIN   (12)
 
-#define MAX_THROTLE     (1900)
-#define THROTlE_OFF     (1500)
-#define MIN_THROTLE     (1100)
+#define MAX_THROTLE     (1832)
+#define MIN_THROTLE     (1148)
 
 // Define the Motor ESC structs
 Servo front_left_esc, front_right_esc, back_right_esc, back_left_esc;
@@ -27,7 +26,7 @@ bool light_on = true;
 // Build buffers and message types
 uint8_t motors_buffer[256];
 size_t motors_buffer_length = sizeof(motors_buffer);
-messaging_MOTORS MOTORS_input = {THROTlE_OFF, THROTlE_OFF, THROTlE_OFF, THROTlE_OFF, 0.0};
+messaging_MOTORS MOTORS_input = {MIN_THROTLE, MIN_THROTLE, MIN_THROTLE, MIN_THROTLE, 0.0};
 
 // Initialize packet serial ports
 PacketSerial jetsonPacketSerial;
@@ -35,12 +34,11 @@ PacketSerial jetsonPacketSerial;
 unsigned long last_time = millis();
 
 void onMotorsRecieved(const uint8_t *buffer, size_t size);
-void displayMessage(messaging_MOTORS &mes);
 void sendMessage(messaging_MOTORS &mes);
 void arm();
 void calibrate();
 void kill();
-
+void blink();
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
@@ -54,10 +52,16 @@ void setup() {
     // Setup PacketSerial to handle communicating from Serial
     Serial.begin(57600);
     while (!Serial) { delay(100); }
+    Serial.println("Connected to Serial");
+
     jetsonPacketSerial.setStream(&Serial);
     jetsonPacketSerial.setPacketHandler(&onMotorsRecieved);
 
-    arm();
+    calibration();
+    Serial.println("Finsihed Calibration");
+
+    // arm();
+    // Serial.println("Armed");
 }
 
 void loop() {
@@ -65,20 +69,8 @@ void loop() {
 
     sendMessage(MOTORS_input);
 
-    if (DEBUG) {
-        displayMessage(MOTORS_input);
-    }
-
-    if ((last_time - millis()) > 1000) {
-        // kill();
-
-        if (light_on) {
-            digitalWrite(LED_PIN, HIGH);
-        }
-        else {
-            digitalWrite(LED_PIN, LOW);
-        }
-        light_on = !light_on;
+    if ((last_time - millis()) > 500) {
+        kill();
     }
 }
 
@@ -91,62 +83,107 @@ void onMotorsRecieved(const uint8_t *buffer, size_t size) {
     if (!status) {
         Serial.printf("\nDecoding failed: %s\n", PB_GET_ERROR(&stream));
     }
-
-    // if (light_on) {
-    //     digitalWrite(LED_PIN, HIGH);
-    // }
-    // else {
-    //     digitalWrite(LED_PIN, LOW);
-    // }
-    // light_on = !light_on;
-
-    last_time = millis();
-}
-
-void displayMessage(messaging_MOTORS &mes) {
-    Serial.println();
-    Serial.print("Front Left: "); Serial.print(mes.front_left);
-    Serial.print("Front Right: "); Serial.println(mes.front_right);
-    Serial.print("Back Right: "); Serial.println(mes.back_right);
-    Serial.print("Back Left: "); Serial.println(mes.back_left);
-    Serial.println();
+    else {
+        if (light_on) {
+            digitalWrite(LED_PIN, LOW);
+        }
+        else {
+            digitalWrite(LED_PIN, HIGH);
+        }
+        light_on = !light_on;
+        last_time = millis();
+    }
 }
 
 void sendMessage(messaging_MOTORS &mes) {
-    /* Create a stream that will write to our buffer. */
-
-    if (THROTlE_OFF < mes.front_left && mes.front_left < MAX_THROTLE) {
+    if (MIN_THROTLE <= mes.front_left && mes.front_left <= MAX_THROTLE) {
         front_left_esc.writeMicroseconds((int) mes.front_left);
     }
-    if (THROTlE_OFF < mes.front_right && mes.front_right < MAX_THROTLE) {
-        front_right_esc.writeMicroseconds((int) mes.front_left);
+    if (MIN_THROTLE <= mes.front_right && mes.front_right <= MAX_THROTLE) {
+        front_right_esc.writeMicroseconds((int)mes.front_right);
     }
-    if (THROTlE_OFF < mes.back_right && mes.back_right < MAX_THROTLE) {
+    if (MIN_THROTLE <= mes.back_right && mes.back_right <= MAX_THROTLE) {
         back_right_esc.writeMicroseconds((int) mes.back_right);
     }
-    if (THROTlE_OFF < mes.back_left && mes.back_left < MAX_THROTLE) {
+    if (MIN_THROTLE <= mes.back_left && mes.back_left <= MAX_THROTLE) {
         back_left_esc.writeMicroseconds((int) mes.back_left);
     }
 }
 
-void arm() {
-    kill();
-    delay(2000);
+void calibration() {
+    // front_left_esc.writeMicroseconds(MAX_THROTLE);
+    // front_right_esc.writeMicroseconds(MAX_THROTLE);
+    // back_right_esc.writeMicroseconds(MAX_THROTLE);
+    // back_left_esc.writeMicroseconds(MAX_THROTLE);
+    // delay(3000);
 
+    // front_left_esc.writeMicroseconds(MIN_THROTLE);
+    // front_right_esc.writeMicroseconds(MIN_THROTLE);
+    // back_right_esc.writeMicroseconds(MIN_THROTLE);
+    // back_left_esc.writeMicroseconds(MIN_THROTLE);
+    // delay(8000);
+
+    front_left_esc.writeMicroseconds(MAX_THROTLE);
+    delay(3000);
+    //Serial.println("throttle down 1");
+    front_left_esc.writeMicroseconds(MIN_THROTLE);
+    delay(3000);
+
+    //Serial.println("throttle up 2") ;
+    front_right_esc.writeMicroseconds(MAX_THROTLE);
+    delay(3000);
+    //Serial.println("throttle down 2");
+    front_right_esc.writeMicroseconds(MIN_THROTLE);
+    delay(3000);
+
+    //Serial.println("throttle up 3") ;
+    back_right_esc.writeMicroseconds(MAX_THROTLE);
+    delay(3000);
+    //Serial.println("throttle down 3");
+    back_right_esc.writeMicroseconds(MIN_THROTLE);
+    delay(3000);
+
+    //Serial.println("throttle up 4") ;
+    back_left_esc.writeMicroseconds(MAX_THROTLE);
+    delay(3000);
+    //Serial.println("throttle down 4");
+    back_left_esc.writeMicroseconds(MIN_THROTLE);
+    delay(8000);
+
+    for (int i = 0; i < 2; i++) {
+        blink();
+    }
+}
+
+void arm() {
     front_left_esc.writeMicroseconds(MAX_THROTLE);
     front_right_esc.writeMicroseconds(MAX_THROTLE);
     back_right_esc.writeMicroseconds(MAX_THROTLE);
     back_left_esc.writeMicroseconds(MAX_THROTLE);
-    delay(7000);
-
-    kill();
     delay(2000);
+
+    front_left_esc.writeMicroseconds(MIN_THROTLE + 200);
+    front_right_esc.writeMicroseconds(MIN_THROTLE + 200);
+    back_right_esc.writeMicroseconds(MIN_THROTLE + 200);
+    back_left_esc.writeMicroseconds(MIN_THROTLE + 200);
+    delay(2000);
+
+    for (int i = 0; i < 3; i++) {
+        blink();
+    }
 }
 
 void kill() {
-    front_left_esc.writeMicroseconds(THROTlE_OFF);
-    front_right_esc.writeMicroseconds(THROTlE_OFF);
-    back_right_esc.writeMicroseconds(THROTlE_OFF);
-    back_left_esc.writeMicroseconds(THROTlE_OFF);
+    front_left_esc.writeMicroseconds(MIN_THROTLE);
+    front_right_esc.writeMicroseconds(MIN_THROTLE);
+    back_right_esc.writeMicroseconds(MIN_THROTLE);
+    back_left_esc.writeMicroseconds(MIN_THROTLE);
 }
 
+void blink() {
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(50);
+}
--- nodes/lqr_hover_controller/lqr_hover_controller_debug.jl
+++ nodes/lqr_hover_controller/lqr_hover_controller_debug.jl
@@ -7,9 +7,8 @@ module LqrHoverControllerDebug
     using ProtoBuf
     using SerialCOBS
 
-    MAX_THROTLE = 1900
-    THROTLE_OFF = 1500
-    MIN_THROTLE = 1100
+    MAX_THROTLE = 1832
+    MIN_THROTLE = 1148
 
     include("$(@__DIR__)/../utils/PubSubBuilder.jl")
     using .PubSubBuilder
@@ -20,66 +19,34 @@ module LqrHoverControllerDebug
 
 
     function motor_commander(motor_pub_ip::String, motor_pub_port::String,
-                             serial_port::String, baud_rate::Int;                     
+                             serial_port::String, baud_rate::Int;
                              freq::Int64=100, debug::Bool=false)
         rate = 1/freq
         ctx = Context(1)
         ard = Arduino(serial_port, baud_rate);
 
         # Setup Filtered state publisher
-        motors = MOTORS(front_left=MIN_THROTLE, front_right=MIN_THROTLE, 
+        motors = MOTORS(front_left=MIN_THROTLE, front_right=MIN_THROTLE,
                         back_right=MIN_THROTLE, back_left=MIN_THROTLE,
                         time=time())
         motors_pub = create_pub(ctx, motor_pub_ip, motor_pub_port)
-        iob = IOBuffer()
         pb = PipeBuffer()
 
+        len = 5 * freq
+        ramp = [MIN_THROTLE:(MAX_THROTLE-MIN_THROTLE)/len:MAX_THROTLE;]
+        ramp = [ramp; reverse(ramp)]
+
         try
-            open(ard) do sp 
-                # motors.front_left = THROTLE_OFF
-                # motors.front_right = THROTLE_OFF
-                # motors.back_right = THROTLE_OFF
-                # motors.back_left = THROTLE_OFF
-
-                # msg_size = writeproto(pb, motors);
-                # message(ard, take!(pb))
-                # sleep(2)
-
-                # motors.front_left = MAX_THROTLE
-                # motors.front_right = MAX_THROTLE
-                # motors.back_right = MAX_THROTLE
-                # motors.back_left = MAX_THROTLE
-
-                # msg_size = writeproto(pb, motors);
-                # message(ard, take!(pb))
-                # sleep(7)
-
-                # motors.front_left = THROTLE_OFF
-                # motors.front_right = THROTLE_OFF
-                # motors.back_right = THROTLE_OFF
-                # motors.back_left = THROTLE_OFF
-
-                # msg_size = writeproto(pb, motors);
-                # message(ard, take!(pb))
-                # sleep(2)
-
+            open(ard) do sp
                 while true
-                    # motors.front_left = Float64((1600 - THROTLE_OFF) * (sin(time() / 2) + 1) + THROTLE_OFF)
-
-                    # println(motors.front_left) 
-                    # motors.front_right = (1600 - MIN_THROTLE) * (sin(time() / 2 + π/2) + 1) + MIN_THROTLE 
-                    # motors.back_right = (1600 - MIN_THROTLE) * (sin(time() / 2 + π) + 1) + MIN_THROTLE 
-                    # motors.back_left = (1600 - MIN_THROTLE) * (sin(time() / 2 + 3π/2) + 1) + MIN_THROTLE 
-
-                    motors.front_left = 1500 #1650
-                    motors.front_right = 1500
-                    motors.back_right = 1500
-                    motors.back_left = 1500
+                # for throt in ramp
+                    motors.front_left = MIN_THROTLE + 1
+                    motors.front_right = MIN_THROTLE + 1
+                    motors.back_right = MIN_THROTLE + 1
+                    motors.back_left = MIN_THROTLE + 1
 
                     msg_size = writeproto(pb, motors);
                     message(ard, take!(pb))
-
-                    publish(motors_pub, motors, iob)
 
                     sleep(rate)
                     GC.gc(false)
@@ -109,7 +76,7 @@ module LqrHoverControllerDebug
 
         fs_pub() = motor_commander(motors_state_ip, motors_state_port,
                                    serial_port, baud_rate;
-                                   freq=20, debug=false)
+                                   freq=50, debug=false)
         fs_thread = Task(fs_pub)
         schedule(fs_thread)
 
