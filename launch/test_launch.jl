8f27edba2c20463a402275fc57f7fe6225c34961
Alexander A Bouman <alex.bouman@gmail.com>
2021-09-14 11:20:50 -0400

    Fixed linux/apple issue

--- launch/test_launch.jl
+++ launch/test_launch.jl
@@ -4,18 +4,20 @@ begin
     Pkg.activate("$(@__DIR__)/..")
 
     include("$(@__DIR__)/../nodes/imu_vicon_publisher/imu_vicon_publisher.jl")
+    include("$(@__DIR__)/../nodes/vicon_relay/vicon_relay_debug.jl")
 
-    imu_vicon_thread = ImuViconPublisher.main(; debug=true)
+    vicon_relay = ViconRelayDebug.main(; debug=true)
+    # imu_vicon_thread = ImuViconPublisher.main(; debug=true)
 
     try
         while true
             sleep(0.1)
 
-            if istaskdone(imu_vicon_thread)
-                fetch(imu_vicon_thread); break
+            if istaskdone(vicon_relay)
+                fetch(vicon_relay); break
             end
         end
     catch e
-        schedule(imu_vicon_thread, InterruptException(), error=true)
+        schedule(vicon_relay, InterruptException(), error=true)
     end
 end
