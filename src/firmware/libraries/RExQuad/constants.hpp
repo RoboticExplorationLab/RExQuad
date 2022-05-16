064b12c03e9cb33d47cd75858899b2f0946df6fa
Brian Jackson <bjack205@gmail.com>
2022-05-16 17:55:42 -0400

    Generate LQR gains

--- src/firmware/libraries/RExQuad/constants.hpp
+++ src/firmware/libraries/RExQuad/constants.hpp
@@ -5,7 +5,8 @@ namespace rexquad {
 constexpr int kNumStates = 13;
 constexpr int kNumErrStates = 12;
 constexpr int kNumInputs = 4;
-constexpr double kHoverInput = 1427.3354062457752;
+// constexpr double kHoverInput = 1427.3354062457752;
+constexpr double kHoverInput = 1449.8408445684368;
 constexpr double kMinInput = 1148.0;
 constexpr double kIdleInput = 1180;
 constexpr double kMaxInput = 1832.0;
---
+++ src/simulator/gen_lqr_gain.jl
@@ -0,0 +1,71 @@
+include("dynamics.jl")
+
+function dlqr(A,B,Q,R; max_iters=200, tol=1e-6, verbose=false)
+    P = Matrix(copy(Q))
+    n,m = size(B)
+    K = zeros(m,n)
+    K_prev = copy(K)
+    
+    for k = 1:max_iters
+        K .= (R + B'P*B) \ (B'P*A)
+        P .= Q + A'P*A - A'P*B*K
+        if norm(K-K_prev,Inf) < tol
+            verbose && println("Converged in $k iterations")
+            return K
+        end
+        K_prev .= K
+    end
+    return K
+end
+
+function generate_LQR_hover_gains(xhover, uhover, dt;
+    Qd = 1*ones(12),
+    Rd = fill(0.1, 4),
+    save_to_file::Bool = true,
+)
+    # Setting x₀ hover state
+    xhover = copy(xhover)
+    uhover = copy(uhover)
+
+    A = ForwardDiff.jacobian(_x->dynamics_rk4(_x, uhover, dt), xhover)
+    B = ForwardDiff.jacobian(_u->dynamics_rk4(xhover, _u, dt), uhover)
+
+    E2 = error_state_jacobian(xhover)
+    Ã = E2' * A * E2
+    B̃ = E2' * B
+
+    # Cost weights
+    Q = Array(Diagonal(Qd))
+    R = Array(Diagonal(Rd))
+
+    # LQR Gain
+    K = dlqr(Ã, B̃, Q, R)
+
+    return K
+end
+
+function print_gains(K, uhover)
+    tofloatstring(x) = string(x) * "f"
+    Kinclude = "const float kFeedbackGain[$(length(K))] = {"
+    Kinclude *= join(tofloatstring.(K), ", ") * "};"
+    uinclude = "const float kUhover[4] = {";
+    uinclude *= join(tofloatstring.(uhover), ", ") * "};"
+    """
+    #pragma once
+    namespace rexquad {
+    $Kinclude
+    $uinclude
+    }  // namespace rexquad
+    """
+end
+
+xhover = [0;0;1; 1;0;0;0; zeros(6)]
+uhover = trim_controls()
+dt = 0.01  # 100 Hz
+
+K = generate_LQR_hover_gains(xhover, uhover, dt)
+
+open(joinpath(@__DIR__, "..", "common", "lqr_constants.hpp"), "w") do f
+    write(f, print_gains(K, uhover))
+end
+
