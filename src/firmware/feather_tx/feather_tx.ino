9cf430834d74e8083f0e3301ced2e5a50de31f71
Brian Jackson <bjack205@gmail.com>
2022-05-17 17:38:16 -0400

    Use common radio init function

--- src/firmware/feather_tx/feather_tx.ino
+++ src/firmware/feather_tx/feather_tx.ino
@@ -1,5 +1,4 @@
-#include <RH_RF69.h>
-
+#include "sensors.hpp"
 #include "pose.hpp"
 
 // Pin Setup
@@ -42,36 +41,7 @@ void Blink(int pin, int delay_ms, int n) {
 // Setup
 /////////////////////////////////////////////
 void setup() {
-  // Serial.begin(256000);
-  // while (!Serial) {
-  //   FastBlink();
-  // }
-  Serial.println("Connected to Transmitter!");
-
-  // Manual reset of radio
-  digitalWrite(RFM69_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM69_RST, LOW);
-  delay(10);
-
-  // Initialize the radio
-  if (!rf69.init()) {
-    Serial.println("RFM69 radio init failed");
-    while (1) {
-      FastBlink();
-    }
-  }
-  if (!rf69.setFrequency(RF69_FREQ)) {
-    Serial.println("setFrequency failed");
-  }
-  Serial.println("RFM69 radio init OK!");
-  rf69.setTxPower(2, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
-
-  // The encryption key has to be the same as the one in the server
-  uint8_t key[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  // rf69.setEncryptionKey(key);
-  rf69.setModemConfig(RH_RF69::GFSK_Rb250Fd250);
+  rexquad::InitRadio(rf69, RF69_FREQ, RFM69_RST, LED_PIN, /*encrypt=*/false);
   digitalWrite(LED_PIN, LOW);
 }
 
